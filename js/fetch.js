67a9968a6d22b011ad37c3203d1ea1e1b9be1f7f
mec2110 <mecdesign.dg@gmail.com>
2022-09-20 09:31:27 -0300

    ajaxyfetch9

--- js/fetch.js
+++ js/fetch.js
@@ -6,12 +6,13 @@ const hechizos = document.getElementById("hechizoElegido")
 fetch("https://hp-api.herokuapp.com/api/spells")
      .then((spell)=>spell.json())
      //.then((json)=>console.log(json))
-     .then((data) => data.forEach((info)=>{
+     .then((data) => 
+        data.forEach((info)=>{
         const li = document.createElement("li")
         li.innerHTML = `
         
         <p>${info.name}</p>
         <p>${info.description}</p>`
 
-        li.append(li)
+        hechizos.append(li)
      }))
