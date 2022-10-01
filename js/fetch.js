30f929168372973da7a0c2900eda0ff7a5dcf3d9
mec2110 <mecdesign.dg@gmail.com>
2022-10-01 13:28:42 -0300

    fetch4

--- js/fetch.js
+++ js/fetch.js
@@ -30,16 +30,33 @@ if(e.target.matches("#search")){
 $characters.innerHTML =`<img class="loader" src="../img/loader.gif" alt="Cargando..."/>`;
 
 let query = e.target.value.toLowerCase();
-api="https://hp-api.herokuapp.com/api/characters=${query}";
-res = await fetch(api);
+api=`https://hp-api.herokuapp.com/api/characters?q=${query}`,
+res = await fetch(api),
 json = await res.json();
 
-//arranco con los condicionales
+//Manipulacion del error
 
-if (!res.ok)throw {status: res.status, statusText:res.statusTex}
+if (!res.ok)throw {status: res.status, statusText:res.statusTex};
+if(json.length === 0){
+    $characters.innerHTML = `<h2>No existen brujas y magos que se llamen: <mark>${query} </mark></h2>`;
+}else{
+    json.forEach(el => {
+$template.querySelector("h3").textContent = el.name;
+$template.querySelector("div").textContent = el.dateOfBirth;
+$template.querySelector("div").textContent = el.house;
+$template.querySelector("img").src = el.image;
+
+
+
+let $clone = personajes.importNode ($template, true);
+$fragment.appendChild($clone);
+    });
+$characters.innerHTML ="";
+    $characters.appendChild($fragment);
+}
         }catch (err){
 console.log(err);
-let message = err.statusText|| "Asì como es Le vi O sa, no leviosaaa...el nombre que ingresaste es incorrecto!";
+let message = err.statusText|| "Asi como es Le vi O sa, no leviosaaa...el nombre que ingresaste es incorrecto!";
 $characters.innerHTML = `<p>Error ${err.status}:${message}</p>`;
         }
     }
