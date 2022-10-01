4da2c696dc42d7a4a9a785cf87552d9e5ca9f026
mec2110 <mecdesign.dg@gmail.com>
2022-10-01 11:53:27 -0300

    search

--- js/fetch.js
+++ js/fetch.js
@@ -1,18 +1,30 @@
 //Fetch API HP
 
 
-const hechizos = document.getElementById("hechizoElegido")
+// const hechizos = document.getElementById("hechizoElegido")
 
-fetch("https://hp-api.herokuapp.com/api/spells")
-     .then((spell)=>spell.json())
-     //.then((json)=>console.log(json))
-     .then((data) => 
-        data.forEach((info)=>{
-        const li = document.createElement("li")
-        li.innerHTML = `
+// fetch("https://hp-api.herokuapp.com/api/spells")
+//      .then((spell)=>spell.json())
+//      //.then((json)=>console.log(json))
+//      .then((data) => 
+//         data.forEach((info)=>{
+//         const li = document.createElement("li")
+//         li.innerHTML = `
         
-        <p>${info.name}</p>
-        <p>${info.description}</p>`
+//         <p>${info.name}</p>
+//         <p>${info.description}</p>`
 
-        hechizos.append(li)
-     }))
+//         hechizos.append(li)
+//      }))
+
+
+const personajes=document,
+$characters =personajes.getElementById("characters"),
+$template=personajes.getElementById("character-template").content,
+$fragment = personajes.createDocumentFragment();
+
+personajes.addEventListener("keypress",async e => {
+if(e.target.matches("#search")){
+    console.log(e.key);
+}
+});
