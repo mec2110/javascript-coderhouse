78d308196fcc44c77ef82a494149523c92685e3f
mec2110 <mecdesign.dg@gmail.com>
2022-10-03 01:17:43 -0300

    puliendo

--- js/fetch.js
+++ js/fetch.js
@@ -1,21 +1,4 @@
-//Fetch API HP
-
-
-// const hechizos = document.getElementById("hechizoElegido")
-
-// fetch("https://hp-api.herokuapp.com/api/spells")
-//      .then((spell)=>spell.json())
-//      //.then((json)=>console.log(json))
-//      .then((data) => 
-//         data.forEach((info)=>{
-//         const li = document.createElement("li")
-//         li.innerHTML = `
-        
-//         <p>${info.name}</p>
-//         <p>${info.description}</p>`
-
-//         hechizos.append(li)
-//      }))
+//Fetch API HP: Permite buscar de qué casa es el personaje ingresado
 
 
 const personajes = document,
